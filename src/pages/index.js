2e927650c7caa6195d45b5e6b55f26d5b16f9226
Jaymin Patel <jem.patel@gmail.com>
2024-01-03 01:23:27 +0530

    [apinger]: add apinger blog article

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/blog">
+            to="/blog/blog">
             Elemprin Blogs - 5min ⏱️
           </Link>
         </div>
